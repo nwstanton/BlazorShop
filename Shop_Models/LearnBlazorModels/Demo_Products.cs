7b9e235b87213578faa4a93558b7c0f60313d742
Natnan Stanton <66800414+nwstanton@users.noreply.github.com>
2022-09-26 12:11:33 -0500

    Functionality changes to Product display

---
+++ Shop_Models/LearnBlazorModels/Demo_Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Models.LearnBlazorModels
+{
+    public class Demo_Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public double Price { get; set; }
+    }
+}
