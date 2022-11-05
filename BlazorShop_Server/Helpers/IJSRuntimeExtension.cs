ac3376c312a72571fb6cf93eefbca10474d9f2c6
Natnan Stanton <66800414+nwstanton@users.noreply.github.com>
2022-11-05 16:00:17 -0500

    Adding in basic JS and ui libraries

---
+++ BlazorShop_Server/Helpers/IJSRuntimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorShop_Server.Helpers
+{
+	public static class IJSRuntimeExtension
+	{
+		public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+		{
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+
+        }
+        public static async ValueTask ToasterFailure(this IJSRuntime jsRuntime, string message)
+		{
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+
+        }
+    }
+}
